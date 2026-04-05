09c1b3404a5416cbc4299f636839697bdbc2ab7f
appminh7-hue <appminh7@gmail.com>
2026-04-05 11:05:09 +0700

    Create index.php for link shortening and location tracking system homepage

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Basic structure for a link shortening and location tracking system
+
+// Include necessary files
+
+// Database connection
+
+// Check if a link is provided
+if(isset($_POST['link'])) {
+    $link = $_POST['link'];
+    // Shorten the link logic here
+}
+
+// Include location tracking logic
+
+?>
